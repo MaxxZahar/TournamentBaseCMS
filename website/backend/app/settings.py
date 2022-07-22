42753791094b4aa92d2435144c7b769969695326
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-07-22 16:59:58 +0300

    basic menu

--- website/backend/app/settings.py
+++ website/backend/app/settings.py
@@ -4,3 +4,13 @@ INSTALLED_APPS += [  # noqa
     'home',
     'tournaments'
 ]
+
+MENU_TYPE_HEADER_MENU = 'header_menu'
+
+MENU_TYPES = {
+    MENU_TYPE_HEADER_MENU: {
+        'title': 'Верхнее меню',
+    },
+}
+
+CHOICE_MENU_TYPES = [(k, v['title']) for k, v in MENU_TYPES.items()]
