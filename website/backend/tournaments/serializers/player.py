90215e6d20eda4e43d4ea3f48ba3c21dc4e80357
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-08-15 14:21:32 +0300

    player number of games

--- website/backend/tournaments/serializers/player.py
+++ website/backend/tournaments/serializers/player.py
@@ -3,6 +3,14 @@ from ..models import PlayerModel
 
 
 class PlayerSerializer(serializers.ModelSerializer):
+    number_of_games = serializers.SerializerMethodField()
+
     class Meta:
         model = PlayerModel
         fields = '__all__'
+
+    def get_number_of_games(self, obj):
+        from ..models import GameModel
+        games_1 = GameModel.objects.filter(player_1=obj)
+        games_2 = GameModel.objects.filter(player_2=obj)
+        return len(games_1) + len(games_2)
