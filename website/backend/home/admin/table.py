26c055ed65f80f69899f9ae39d8d76e0da7ac881
maxxzahar <argentum47>
2022-09-07 03:27:38 +0300

    test git

--- website/backend/home/admin/table.py
+++ website/backend/home/admin/table.py
@@ -8,5 +8,4 @@ class TableModelAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         file = request.FILES['table'].readlines()
         handling_on_save(file)
-
         super().save_model(request, obj, form, change)
