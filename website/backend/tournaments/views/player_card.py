f712842814e057de0756ffd1f4cf36360731b7ff
maxxzahar <argentum47>
2022-09-20 19:08:10 +0300

    player card tournaments pagination

--- website/backend/tournaments/views/player_card.py
+++ website/backend/tournaments/views/player_card.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from ..models import PlayerModel, GameModel
-from django.db.models import Q, Count, Max
-from ..utilities import frequency_dict, gain_opponents_list
-from ..settings import NUMBER_OF_ITEMS_IN_LIST_OF_OPPONENTS
+from django.db.models import Q
+from ..utilities import frequency_dict, gain_opponents_list, paginate, get_page
+from ..settings import NUMBER_OF_ITEMS_IN_LIST_OF_OPPONENTS, PLAYER_CARD_TOURNAMENTS_PER_PAGE
 
 
 def player(request, player_id):
@@ -15,11 +15,21 @@ def player(request, player_id):
     q = GameModel.objects.filter(Q(player_1=player_ex) | Q(player_2=player_ex)).values('player_1', 'player_2', 'result')
     freq = frequency_dict(q, player_id)
     res = gain_opponents_list(freq, NUMBER_OF_ITEMS_IN_LIST_OF_OPPONENTS)
+    queryset = player_ex.get_tournaments()
+    pages = paginate(queryset, PLAYER_CARD_TOURNAMENTS_PER_PAGE)
+    page = get_page(pages, request)
+    tournaments = page.get('queryset')
+    page_number = page.get('page_number')
+    page_range = list(range(1, len(pages) + 1))
+
     context = {
         'player': player_ex,
+        'tournaments': tournaments,
         'number_of_games': number_of_games,
         'number_of_wins': number_of_wins,
         'winning_percentage': winning_percentage,
         'opponents': res,
+        'page': page_number,
+        'page_range': page_range,
     }
     return render(request, 'pages/player_card.html', context)
