101fb8afa30a224f1cd337e1b0070bc39c3e9e3d
maxxzahar <argentum47>
2022-09-22 02:26:13 +0300

    google captcha

--- website/backend/app/settings.py
+++ website/backend/app/settings.py
@@ -4,7 +4,12 @@ INSTALLED_APPS += [  # noqa
     'home',
     'tournaments',
     'api',
+    'captcha',
 ]
+
+RECAPTCHA_PUBLIC_KEY = '6LcJ5hoiAAAAAChq77yt-6l15YM9E-cgaUkLiuql'
+RECAPTCHA_PRIVATE_KEY = '6LcJ5hoiAAAAANrBj3CorlxffQJKbcgVbX8iReM6'
+SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
 
 MENU_TYPE_HEADER_MENU = 'header_menu'
 
--- website/backend/home/forms/table.py
+++ website/backend/home/forms/table.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
 from ..models import TableModel
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Checkbox
 
 
 class TableForm(ModelForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
+
     class Meta:
         model = TableModel
         fields = '__all__'
